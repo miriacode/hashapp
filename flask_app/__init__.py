dde4e8a65c7fac09c35f1690fe97ce121ddf7768
ojitos369 <ojitos369@gmail.com>
2023-03-03 07:32:10 -0600

    agregando ruta de login

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 app.secret_key = "My secret key"
 
 app.config['UPLOAD_FOLDER'] = 'flask_app/static/assets/images'
+
+@app.route('/')
+def home():
+    return render_template('sign-in.html')
 
 # https://www.youtube.com/watch?v=LC-14nHGaMY
